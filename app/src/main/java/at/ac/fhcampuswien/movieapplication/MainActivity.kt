1561a1ee4b87959400da017559d6be0cdd3fe6b5
leonardo1710 <leon.freudenthaler@fh-campuswien.ac.at>
2022-03-07 13:13:19 +0100

    1. task

--- app/src/main/java/at/ac/fhcampuswien/movieapplication/MainActivity.kt
+++ app/src/main/java/at/ac/fhcampuswien/movieapplication/MainActivity.kt
@@ -3,13 +3,17 @@ package at.ac.fhcampuswien.movieapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import at.ac.fhcampuswien.movieapplication.ui.theme.MovieApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +23,10 @@ class MainActivity : ComponentActivity() {
             MovieApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    //modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MovieRow(movie = "Harry Potter")
                 }
             }
         }
@@ -30,14 +34,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MovieRow(movie: String) {
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(130.dp),
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = 6.dp
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Surface(modifier = Modifier
+                .size(100.dp)
+                .padding(12.dp)
+            ) {
+                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "movie pic")
+            }
+
+            Text(text = movie, style = MaterialTheme.typography.h6)
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MovieApplicationTheme {
-        Greeting("Android")
+        MovieRow("Marry Poppins")
     }
 }
