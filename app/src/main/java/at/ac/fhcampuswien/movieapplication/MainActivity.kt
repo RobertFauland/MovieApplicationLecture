449a2cc3bb5c7b92a5f7030a6bb39359c1d9feac
leonardo1710 <leon.freudenthaler@fh-campuswien.ac.at>
2022-03-08 09:31:43 +0100

    3. task

--- app/src/main/java/at/ac/fhcampuswien/movieapplication/MainActivity.kt
+++ app/src/main/java/at/ac/fhcampuswien/movieapplication/MainActivity.kt
@@ -3,13 +3,21 @@ package at.ac.fhcampuswien.movieapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import at.ac.fhcampuswien.movieapplication.models.Movie
+import at.ac.fhcampuswien.movieapplication.models.getMovies
 import at.ac.fhcampuswien.movieapplication.ui.theme.MovieApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +27,10 @@ class MainActivity : ComponentActivity() {
             MovieApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    //modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MainContent()
                 }
             }
         }
@@ -30,14 +38,62 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainContent(movies: List<Movie> = getMovies()){
+    LazyColumn {
+        /*
+        item(){
+            Text(text = "Header")
+        }
+
+         */
+        items(movies){ movie ->
+            MovieRow(movie)
+        }
+
+        /*
+        items(5){
+            Text(text = "mytext")
+        }
+         */
+    }
+}
+
+@Preview
+@Composable
+fun MovieRow(movie: Movie = getMovies()[0]) {
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(130.dp),
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = 6.dp
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Surface(
+                modifier = Modifier
+                    .size(130.dp)
+                    .padding(12.dp),
+                elevation = 6.dp
+            ) {
+                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "movie pic")
+            }
+
+            Column() {
+                Text(text = "${movie.title}", style = MaterialTheme.typography.h5)
+                Text(text = "Director: ${movie.director}", style = MaterialTheme.typography.caption)
+                Text(text = "Released: ${movie.year}", style = MaterialTheme.typography.caption)
+            }
+
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MovieApplicationTheme {
-        Greeting("Android")
+        MainContent()
     }
 }
