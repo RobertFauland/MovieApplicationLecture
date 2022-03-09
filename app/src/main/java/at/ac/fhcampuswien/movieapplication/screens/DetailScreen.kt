ace2dfbb3b42ffbd9aca54ff231792793bb00a04
leonardo1710 <leon.freudenthaler@fh-campuswien.ac.at>
2022-03-09 10:28:51 +0100

    task 2 + 3

--- app/src/main/java/at/ac/fhcampuswien/movieapplication/screens/DetailScreen.kt
+++ app/src/main/java/at/ac/fhcampuswien/movieapplication/screens/DetailScreen.kt
@@ -1,10 +1,71 @@
 package at.ac.fhcampuswien.movieapplication.screens
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import at.ac.fhcampuswien.movieapplication.models.Movie
+import at.ac.fhcampuswien.movieapplication.models.getMovies
+import at.ac.fhcampuswien.movieapplication.widgets.MovieRow
+
+fun filterMovie(movieId: String?): Movie {
+    return getMovies().filter { it.id == movieId}[0]
+}
 
 @Composable
 fun DetailScreen(navController: NavController, movieId: String?){
-    Text(text = "Hello detailscreen $movieId")
+    val movie = filterMovie(movieId = movieId)
+
+    Scaffold(
+        topBar = {
+            TopAppBar(elevation = 3.dp) {
+                Row() {
+                    Icon(imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "Arrow back",
+                        modifier = Modifier.clickable {
+                            navController.popBackStack()    // go back to last screen
+                        }
+                    )
+
+                    Spacer(modifier = Modifier.width(20.dp))
+
+                    Text(text = movie.title)
+                }
+
+            }
+        }
+    ) {
+        MainContent(movie = movie)
+    }
+}
+
+@Composable
+fun MainContent(movie: Movie){
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+
+            MovieRow(movie = movie)
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Divider()
+
+            Text(text = "Movie Images", style = MaterialTheme.typography.h5)
+        }
+    }
 }
